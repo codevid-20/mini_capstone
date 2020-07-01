6e212c7f2bbfdb58d23fb7b113544830fb50f2ed
brian <riceb53@gmail.com>
2020-06-30 21:44:26 -0400

    all products route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,8 @@
 class Api::ProductsController < ApplicationController
+  def all
+    # get all of the products
+    @products = Product.all
+    # end them to index.json.jb
+    render 'index.json.jb'
+  end
 end
