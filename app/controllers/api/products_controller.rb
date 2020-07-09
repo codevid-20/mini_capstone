93a5ee75994a86b6c293814480575d3b2f223ee5
brian <riceb53@gmail.com>
2020-07-08 21:18:35 -0400

    make create action, add happy sad paths

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,6 +3,23 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'index.json.jb'
   end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description],
+    )
+    if @product.save
+      # happy path
+      render 'show.json.jb'
+    else
+      # sad path
+      # render 'errors.json.jb', status: :unprocessable_entity
+      render json: {errors: @product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
   
   # def all
   #   # get all of the products
