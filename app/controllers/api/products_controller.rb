5e137e675d67098b2777f0feb6bacf213f155138
brian <riceb53@gmail.com>
2020-07-09 21:23:55 -0400

    add search

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.where("name LIKE ?", "%#{params[:search]}%")
     render 'index.json.jb'
   end
 
