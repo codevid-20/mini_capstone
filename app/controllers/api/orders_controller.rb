5a7fd63549b7eef37a59046c5fc081a153620553
brian <riceb53@gmail.com>
2020-07-15 19:48:08 -0400

    add orders show action

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -27,4 +27,9 @@ class Api::OrdersController < ApplicationController
     @order.save
     render 'show.json.jb'
   end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
