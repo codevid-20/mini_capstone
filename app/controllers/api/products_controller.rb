e53c095867aa579b2042de7f757dca90051cec88
brian <riceb53@gmail.com>
2020-07-16 20:52:53 -0400

    add ability to search by category in products index

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,19 +2,28 @@ class Api::ProductsController < ApplicationController
   before_action :authenticate_admin, only: [:create]
   
   def index
-    @products = Product.where("name LIKE ?", "%#{params[:search]}%")
+    # @products = Product.where("name LIKE ?", "%#{params[:search]}%")
 
-    if params[:discount] == "true"
-      @products = @products.where('price < 10')
-    end
+    # if params[:discount] == "true"
+    #   @products = @products.where('price < 10')
+    # end
 
-    if params[:sort] == "price" && params[:sort_order] == 'desc'
-      @products = @products.order(:price => :desc)
-    elsif params[:sort] == "price"
-      @products = @products.order(:price)    
-    else
-      @products = @products.order(:id)
-    end
+    # if params[:sort] == "price" && params[:sort_order] == 'desc'
+    #   @products = @products.order(:price => :desc)
+    # elsif params[:sort] == "price"
+    #   @products = @products.order(:price)    
+    # else
+    #   @products = @products.order(:id)
+    # end
+
+
+    # find the correct category
+    category = Category.find_by(name: params[:category])
+    # find all the products for that cagegory
+    
+    
+    
+    @products = category.products
 
     
 
